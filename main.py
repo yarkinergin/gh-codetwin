dd2a51e9e07cd7f789ea31db76596d5f2f91a844
Yarkın Ergin <yarkin.ergin@ug.bilkent.edu.tr>
2024-04-23 15:29:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+import os
+
+def iterFile(name):
+    with open(name) as f:
+        seen = set()
+        for line in f:
+            line_lower = line.lower()
+            if line_lower in seen and line_lower.strip() != "":
+                print("*******" ,name , line)
+            else:
+                seen.add(line_lower)
+
+def iterDir(directory):
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        print(filename)
+        if (os.path.isfile(filename)):
+            iterFile(filename)
+        elif (os.path.isdir(filename) and filename[0] != "."):
+            iterDir(filename)
+
 print("sasda")
-with open('file') as f:
-    seen = set()
-    for line in f:
-        line_lower = line.lower()
-        if line_lower in seen:
-            print(line)
-        else:
-            seen.add(line_lower)
+directory = os.fsencode(os.getcwd())
+print(directory)
+iterDir(directory)
+    
