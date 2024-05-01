e9451a101fbf30b7805bb5850e6f298c7f5ec977
Yarkın Ergin <yarkin.ergin@ug.bilkent.edu.tr>
2024-05-01 20:31:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,25 @@ import os
 def iterFile(name):
     with open(name) as f:
         seen = set()
+        count = 0
         for line in f:
+            count += 1
             line_lower = line.lower()
             if line_lower in seen and line_lower.strip() != "":
-                print("*******" ,name , line)
+                print("File:", name, " -> ", str(count) + ": ", line)
             else:
                 seen.add(line_lower)
 
 def iterDir(directory):
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
-        print(filename)
-        if (os.path.isfile(filename)):
+        if (os.path.isfile(filename) and filename != "gh-codetwin"):
             iterFile(filename)
         elif (os.path.isdir(filename) and filename[0] != "."):
+            print(filename)
             iterDir(filename)
 
-print("sasda")
+print("Start:")
 directory = os.fsencode(os.getcwd())
 print(directory)
 iterDir(directory)
