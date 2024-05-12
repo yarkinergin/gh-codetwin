e2b95a8cb7c87673808bc1400795c284122c9b9b
Yarkın Ergin <yarkin.ergin@ug.bilkent.edu.tr>
2024-05-12 12:26:42 +0300

    fix filename

--- main.py
+++ main.py
@@ -10,22 +10,24 @@ def iterFile(name):
             count += 1
             line_lower = line.lower()
             if line_lower in seen and line_lower.strip() != "":
-                print("File:", name, " -> ", str(count) + ": ", line)
+                print("File:", os.fsdecode(os.path.basename(name)), " -> ", str(count) + ": ", line)
             else:
                 seen.add(line_lower)
 
 def iterDir(directory):
     for file in os.listdir(directory):
-        filename = os.fsdecode(file)
+        filename = os.path.join(directory, file)
+        print(os.path.join(directory, file))
         if (os.path.isfile(filename) and filename != "gh-codetwin"):
             iterFile(filename)
         elif (os.path.isdir(filename) and filename[0] != "."):
             print(filename)
             iterDir(filename)
-print(sys.argv[1])
+
 print("Start:")
 #directory = os.fsencode(os.getcwd())
 directory = os.fsencode(sys.argv[1])
+print(directory)
 #print(directory)
 iterDir(directory)
 
