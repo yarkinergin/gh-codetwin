0bc5da710256a8190ff8d0fa4fead9b3d4eb2199
Yarkın Ergin <yarkin.ergin@ug.bilkent.edu.tr>
2024-05-13 20:13:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ def iterDir(directory):
 print("Start:\n")
 #directory = os.fsencode(os.getcwd())
 directory = os.fsencode(sys.argv[1])
-#print(directory)
 iterDir(directory)
+#print(directory)
 
 path = os.path.join(sys.argv[1], ".github")
 if(not os.path.exists(path)):
